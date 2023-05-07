920b99a1ef5cdca60895336baf6a8ce01adc1bd3
KwakYounHo <kwakyh951@gmail.com>
2023-05-07 23:04:35 +0900

    웹팩의 정의파일에 ESM방식으로 정의

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@
 5. webpack.conifg.js
   - 웹팩 정의 파일은 특이하게 js파일을 요구한다.
   - 보통은 JSON, yaml, bat파일을 요구한다고 한다.
+6. webpack 파일에서 정의를 ESM방식으로 모듈로 만들어 보았다
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// config파일의 위치는 항상 최상위경로
+// ESM 방식으로 모듈을 만들어 볼 계획
+
+export default {
+  entry  : "진입점 파일, 메인 파일이름",
+  output : {
+    filename : "번들 할 파일이름"
+  }
+}
